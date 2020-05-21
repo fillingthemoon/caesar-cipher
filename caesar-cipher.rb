2d0a467c706c32c4d44fd924af589ee232aa5a7c
Philemon Heng <phil.hsy@outlook.com>
2020-05-21 20:00:48 +0800

    added caeser-cipher

---
+++ caesar-cipher.rb
@@ -0,0 +1,27 @@
+def caesar_cipher(string, num)
+  upper = []
+  lower = []
+
+  for i in 65..90 do
+    upper.push(i.chr)
+  end
+
+  for i in 97..122 do
+    lower.push(i.chr)
+  end
+
+  newArr = []
+  string.chars.each do |char|
+    if upper.include?(char)
+      newChar = (upper.index(char) + num) % 26
+      newArr.push(upper[newChar])
+    elsif lower.include?(char)
+      newChar = (lower.index(char) + num) % 26
+      newArr.push(lower[newChar])
+    else
+      newArr.push(char)
+    end
+  end
+  
+  newArr.join
+end
